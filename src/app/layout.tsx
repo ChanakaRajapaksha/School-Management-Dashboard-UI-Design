9862de9e93c1a07d4063e187e1f036fdaedcc01e
IT20137250 <it20137250@my.sliit.lk>
2024-10-02 01:06:29 +0530

    change the layout.tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Add the favicon */}
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
